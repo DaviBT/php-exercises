1833bac214540a1b92dd0519b27c2e69a4f58f9b
DaviBT <davi.cesarbt@gmail.com>
2023-02-09 21:03:53 -0300

    exercise using variables and const's

---
+++ ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php 
+        $name = "Davi";
+        $age = 16;
+        const COUNTRY = "Brazil";
+
+        echo "Hello! My name is $name and I'm $age years old. I am from " . COUNTRY;
+    ?>
+</body>
+</html>
