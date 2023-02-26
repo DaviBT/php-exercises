1309371bc31506936d98c56287da7950f7d71dfd
DaviBT <davi.cesarbt@gmail.com>
2023-02-26 00:04:08 -0300

    primitive data type exercise created

---
+++ ex004/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primitive data types</title>
+</head>
+<body>
+    <h1>Primitive data types</h1>
+    <?php
+        // 0x = hexadecimal
+        // 0b = binary
+        // 0 octal
+        $num = 0x1A;
+        echo "The number is $num";
+    ?>
+</body>
+</html>
